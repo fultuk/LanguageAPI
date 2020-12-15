19a532df908f8521f8c13e3cf6fd58d6e889510d
0utplay <aldin@sijamhodzic.de>
2020-12-15 16:49:52 +0100

    Add new license to every class

--- Core/src/main/java/de/tentact/languageapi/api/DefaultLanguageAPI.java
+++ Core/src/main/java/de/tentact/languageapi/api/DefaultLanguageAPI.java
@@ -1,9 +1,28 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 0utplay
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package de.tentact.languageapi.api;
-/*  Created in the IntelliJ IDEA.
-    Created by 0utplay | Aldin Sijamhodzic
-    Datum: 25.04.2020
-    Uhrzeit: 16:52
-*/
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
@@ -408,6 +427,7 @@ public abstract class DefaultLanguageAPI extends LanguageAPI {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+
         return false;
     }
 
